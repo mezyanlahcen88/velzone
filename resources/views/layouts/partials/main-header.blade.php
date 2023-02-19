7bb61cc1f0d55176a0b0e0f4b833058517063239
mezyanlahcen88 <mezyan.lahcen88@gmail.com>
2023-02-19 19:50:54 +0100

    delete assets

--- resources/views/layouts/partials/main-header.blade.php
+++ resources/views/layouts/partials/main-header.blade.php
@@ -189,7 +189,7 @@
                             <span class="text-start ms-xl-2">
                                 <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{Auth::user()->name}}
                                     </span>
-                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">{{Auth::user()->name}}</span>
+                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">{{Auth::user()->email}}</span>
                             </span>
                         </span>
                     </button>
